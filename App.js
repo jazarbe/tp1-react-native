7523e36e26972fff619dbec1bdac9d098fb97e53
DIANA IRENE PARK <49377760@ort>
2026-06-09 08:33:35 -0300

    codigo base hecho

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { img } from './assets/coca-cola-icon.png'
+import { SafeAreaView } from 'react-native/types_generated/index';
+import { TextInput } from 'react-native/types_generated/index';
+import { Button } from 'react-native/types_generated/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <StatusBar style="backgroundColor: #612FFC" ><Text>Login App (Park, Arias)</Text></StatusBar>
+        <Image source={{ url: img }} />
+        <TextInput style={[styles.text, styles.input]}></TextInput>
+        <TextInput style={[styles.text, styles.input]}></TextInput>
+        <Button style={"backgroundColor: #8E5DFB, color: #fff"} />
+        <Text style={styles.text}>Olvidaste la clave?</Text>
+        <Text style={styles.text}>Crear Cuenta</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +28,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#000'
+  },
+  input: {
+    borderColor: '#8E5DFB',
+    borderRadius: '5px'
+  }
 });
